aaff4d94bb9ba82c922f72df969261543bd158e4
bamboolicious <kasit008@gmail.com>
2020-02-15 00:17:03 +0700

    Matching Game is done

--- Assets/Scripts/AnswerZone.cs
+++ Assets/Scripts/AnswerZone.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class AnswerZone : MonoBehaviour
 {
---
+++ Assets/Scripts/AnswerZoneMatch.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class AnswerZoneMatch : MonoBehaviour
+{
+    // //public bool isCorrectWord = false;
+    // public string displayText;
+    public TextMeshProUGUI answerText;
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MatchingController.OnPlayerEnterZone(answerText.text);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MatchingController.OnPlayerExitZone();
+        }
+    }
+    
+}
