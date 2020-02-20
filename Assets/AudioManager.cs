7951c58843a581d2e7527019cea7630c939e1a3a
bamboolicious <kasit008@gmail.com>
2020-02-21 00:23:28 +0700

    2/21/2020

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    private AudioSource audioSource;
+    private string audioString;
+    private AudioClip playerJump, playerHit, playerWalk, playerIncorrect, playerCorrect, onGameEnd, buttonPressed;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Instance = this;
+    }
+
+    public void PlayJump()
+    {
+        audioSource.PlayOneShot(playerJump);
+    }
+
+     public void PlayHit()
+    {
+        audioSource.PlayOneShot(playerHit);
+    }
+    
+     public void PlayWalk()
+    {
+        audioSource.PlayOneShot(playerWalk);
+    }
+
+     public void PlayIncorrect()
+    {
+        audioSource.PlayOneShot(playerIncorrect);
+    }
+     public void PlayCorrect()
+    {
+        audioSource.PlayOneShot(playerCorrect);
+    }
+     public void PlayGameEnd()
+    {
+        audioSource.PlayOneShot(onGameEnd);
+    }
+     public void PlayButtonPress()
+    {
+        audioSource.PlayOneShot(buttonPressed);
+    }
+
+}
